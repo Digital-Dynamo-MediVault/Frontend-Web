8ad0f9a82ed7da4f46e15b4b0d24a83604757ce9
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2024-01-27 22:51:47 +0530

    added cases

--- src/components/hospital/admin/newcase.jsx
+++ src/components/hospital/admin/newcase.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
+import { apiUrl, contractAddress } from "../../../constants/apiUrl";
+import axios from "axios";
+import { Web3 } from 'web3';
+import contractABI from '../../../constants/abi.json';
+
 
 function Newcase({ handleClosePopups }) {
   const [formData, setFormData] = useState({
-    patientid: "",
+    pId: "",
     doctor: "",
     specialization: "",
-    description: "",
+    problemDescription: "",
     symptoms: "",
   });
 
-  const doctorNames = [
-    "Dr. Smith",
-    "Dr. Johnson",
-    "Dr. Williams",
-    "Dr. Jones",
-    "Dr. Brown",
-    "Dr. Davis",
-  ];
+  const [specialization, setSpecialization] = useState('');
+  const [doctorData, setDoctorData] = useState(null);
+
+  const handleSpecializationChange = async (event) => {
+    const newSpecialization = event.target.value;
+    setSpecialization(newSpecialization);
+
+    try {
+      const response = await axios.post(`${apiUrl}/case/doctor`, { specialization: newSpecialization });
+      setDoctorData(response.data.data);
+      console.log(response.data.data);
+    } catch (error) {
+      console.error('Error fetching doctors:', error);
+      setDoctorData(null);
+    }
+  };
 
   const specializations = [
     "Cardiologist",
@@ -36,8 +49,89 @@ function Newcase({ handleClosePopups }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add your logic for form submission here
+
     console.log("Form data submitted:", formData);
+    axios.post(`${apiUrl}/case/newcase`, formData).then((res) => {
+      // console.log(res);
+      if (res.status === 200) {
+        window.ethereum.request({ method: 'eth_requestAccounts' }).then(async function (accounts) {
+          console.log(accounts[0]);
+          // axios.post(`${apiUrl}/patient/activate`, { email: queryParams.get('email'), password: password, metamaskAddress: accounts[0] }).then(async function (response) {
+          //   console.log(response.data.user);
+          try {
+            // Request account access if needed
+            const web3 = new Web3(new Web3.providers.HttpProvider(`https://polygonzkevm-testnet.g.alchemy.com/v2/lsmKHpFVS4DmS_WzTTQd1S_aXgc5jRBZ`));
+
+
+            const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+            await window.ethereum.enable();
+
+
+
+            const nonce = await web3.eth.getTransactionCount(accounts[0], 'pending');
+            const gasPrice = await web3.eth.getGasPrice();
+            const gasLimit = 28500000;
+            console.log("res", res.data.data.pId)
+            const rawTransaction = {
+              from: accounts[0],
+              nonce: '0x' + nonce.toString(16),
+              gasPrice: '0x' + parseInt(gasPrice).toString(16),
+              gasLimit: '0x' + parseInt(gasLimit).toString(16),
+              to: contractAddress,
+              value: '0x0',
+              data: contractInstance.methods.addCase(
+                res.data.data.pId,
+                res.data.data.specialization,
+                res.data.data.problemDescription,
+                res.data.data.doctor,
+                res.data.data.symptoms,
+              ).encodeABI(),
+            };
+
+            // Sign transaction with MetaMask
+            const signedTransaction = await window.ethereum.request({
+              method: 'eth_sendTransaction',
+              params: [rawTransaction],
+            });
+
+            console.log('Transaction sent to MetaMask. Please confirm in MetaMask.');
+
+            // Wait for the transaction receipt with a timeout of 3 minutes (adjust as needed)
+            const timeout = 180000; // 3 minutes in milliseconds
+            const startTime = Date.now();
+
+            let receipt;
+
+            while (Date.now() - startTime < timeout) {
+              try {
+                receipt = await web3.eth.getTransactionReceipt(signedTransaction);
+              } catch (error) {
+                console.error('Error fetching transaction receipt:', error);
+              }
+
+              if (receipt) {
+                console.log('Transaction receipt:', receipt);
+                handleClosePopups();
+                break; // Exit the loop if receipt is found
+              } else {
+                await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second before checking again
+              }
+            }
+
+            if (!receipt) {
+              console.error('Transaction not found within the timeout period.');
+            }
+          } catch (error) {
+            console.error('Error interacting with contract:', error);
+          }
+        })
+        // })
+
+
+      } else {
+        alert("Error creating case.");
+      }
+    })
   };
 
   return (
@@ -57,7 +151,7 @@ function Newcase({ handleClosePopups }) {
             >
               <div className="flex flex-col items-center justify-center">
                 <label
-                  htmlFor="patientid"
+                  htmlFor="pId"
                   className="font-inter font-bold text-[1.2rem]"
                   style={{ textAlign: "left" }}
                 >
@@ -65,9 +159,9 @@ function Newcase({ handleClosePopups }) {
                 </label>
                 <input
                   type="text"
-                  id="patientid"
-                  name="patientid"
-                  value={formData.patientid}
+                  id="pId"
+                  name="pId"
+                  value={formData.pId}
                   onChange={handleChange}
                   className="h-[4vh] w-[15vw] border border-black rounded-md"
                 />
@@ -84,7 +178,10 @@ function Newcase({ handleClosePopups }) {
                   id="specialization"
                   name="specialization"
                   value={formData.specialization}
-                  onChange={(e) => handleChange(e)}
+                  onChange={(e) => {
+                    handleChange(e)
+                    handleSpecializationChange(e)
+                  }}
                   className="h-[4vh] w-[15vw] border border-black rounded-md"
                 >
                   <option value="">Specialization</option>
@@ -97,16 +194,16 @@ function Newcase({ handleClosePopups }) {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <label
-                  htmlFor="description"
+                  htmlFor="problemDescription"
                   className="text-left font-inter font-bold text-[1.2rem]"
                 >
                   DESCRIPTION
                 </label>
                 <input
                   type="text"
-                  id="description"
-                  name="description"
-                  value={formData.description}
+                  id="problemDescription"
+                  name="problemDescription"
+                  value={formData.problemDescription}
                   onChange={handleChange}
                   className="h-[4vh] w-[15vw] border border-black rounded-md"
                 />
@@ -126,9 +223,9 @@ function Newcase({ handleClosePopups }) {
                   className="h-[4vh] w-[15vw] border border-black rounded-md"
                 >
                   <option value="">Select a doctor</option>
-                  {doctorNames.map((doctor, index) => (
-                    <option key={index} value={doctor}>
-                      {doctor}
+                  {doctorData && doctorData.map((doctor, index) => (
+                    <option key={index} value={doctor.metamaskAddress}>
+                      {doctor.name}
                     </option>
                   ))}
                 </select>
