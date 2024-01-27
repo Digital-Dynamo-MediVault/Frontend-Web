ea3e1c7b296f6beb2c499fbca655c71637873c4d
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2024-01-27 19:11:31 +0530

    fixes

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,3 +1,3 @@
-export const contractAddress = "0x25CE0ada87e4D669929f8A365F3578f630a30255"
+export const contractAddress = "0xA58CCCf54d2ED60afe477527A0B7124b2375e04c"
 // export const apiUrl = "http://localhost:5000"
 export const apiUrl = "https://embarrassed-twill-dog.cyclic.app"
